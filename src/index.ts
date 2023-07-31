3011383a09aa48b1718c0fad4d3683dc48aa12f6
Bhaumik1005 <bhaumikjsh@gmail.com>
2023-07-31 10:18:33 +0530

    Create Auth functions

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,50 @@
+import crypto from "node:crypto";
+import cryptoJS from "crypto-js";
+
 class OpenAuth {
-  generateToken(): string {
-    return "jkhkjhasdadshkajhds";
+  static generateToken() {
+    return crypto
+      .createHash("sha512")
+      .update(Date.now().toString())
+      .digest("base64");
+  }
+
+  generateClientIdAndSecret(secret: string) {
+    const timestamp = Date.now().toString();
+    const token = crypto
+      .createHmac("sha256", secret)
+      .update(timestamp)
+      .digest("hex");
+    const clientId = token.slice(0, 20);
+    let clientSecret = token.slice(20, token.length);
+    const encryptedTime = cryptoJS.AES.encrypt(timestamp, secret).toString();
+    clientSecret += encryptedTime;
+
+    return {
+      clientId: clientId,
+      clientSecret: clientSecret,
+    };
+  }
+
+  verifyClient(clientId: string, clientSecret: string, secret: string) {
+    let timestamp = clientSecret.slice(
+      clientSecret.length - 44,
+      clientSecret.length
+    );
+    timestamp = cryptoJS.AES.decrypt(timestamp, secret).toString(
+      cryptoJS.enc.Utf8
+    );
+    const token = crypto
+      .createHmac("sha256", secret)
+      .update(timestamp)
+      .digest("hex");
+
+    clientSecret = clientId + clientSecret.slice(0, clientSecret.length - 44);
+    if (token === clientSecret) {
+      return OpenAuth.generateToken();
+    }
+    throw new Error("Authentication failed");
   }
 }
+
 module.exports = OpenAuth;
