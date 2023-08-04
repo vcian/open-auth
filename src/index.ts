4f56fe8aadf0b6c0deba32244287fe35e06823f4
Bhaumik1005 <bhaumikjsh@gmail.com>
2023-08-04 17:26:55 +0530

    Create all functions logic file and add validation token function

--- src/index.ts
+++ src/index.ts
@@ -1,50 +1,37 @@
-import crypto from "node:crypto";
-import cryptoJS from "crypto-js";
+import {
+  createClientIdAndSecret,
+  createToken,
+  verifyClientId,
+  verifyClientSecret,
+} from "./utils/logic";
 
 class OpenAuth {
-  static generateToken() {
-    return crypto
-      .createHash("sha512")
-      .update(Date.now().toString())
-      .digest("base64");
+  static generateToken(secret: string) {
+    return createToken(secret);
   }
 
   generateClientIdAndSecret(secret: string) {
-    const timestamp = Date.now().toString();
-    const token = crypto
-      .createHmac("sha256", secret)
-      .update(timestamp)
-      .digest("hex");
-    const clientId = token.slice(0, 20);
-    let clientSecret = token.slice(20, token.length);
-    const encryptedTime = cryptoJS.AES.encrypt(timestamp, secret).toString();
-    clientSecret += encryptedTime;
-
-    return {
-      clientId: clientId,
-      clientSecret: clientSecret,
-    };
+    return createClientIdAndSecret(secret);
   }
 
   verifyClient(clientId: string, clientSecret: string, secret: string) {
-    let timestamp = clientSecret.slice(
+    const timestamp = clientSecret.slice(
       clientSecret.length - 44,
       clientSecret.length
     );
-    timestamp = cryptoJS.AES.decrypt(timestamp, secret).toString(
-      cryptoJS.enc.Utf8
-    );
-    const token = crypto
-      .createHmac("sha256", secret)
-      .update(timestamp)
-      .digest("hex");
-
-    clientSecret = clientId + clientSecret.slice(0, clientSecret.length - 44);
-    if (token === clientSecret) {
-      return OpenAuth.generateToken();
+    const token = verifyClientId(timestamp, secret);
+    const ClientSecretData = verifyClientSecret(clientId, clientSecret);
+    if (token === ClientSecretData) {
+      return OpenAuth.generateToken(secret);
     }
     throw new Error("Authentication failed");
   }
+
+  verifyToken = (token: string, secret: string) => {
+    const generateToken = createToken(secret);
+    if (generateToken === token) return true;
+    return false;
+  };
 }
 
 module.exports = OpenAuth;
