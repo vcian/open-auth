28ad932a5100ea9492fdaa4ed202e373965b8b9a
bhanudatsinhjhala <jhalabhanudattsinh4090@gmail.com>
2023-10-10 16:41:06 +0530

    (feat): Added type error throw for class methods

--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,49 @@
 import {
-  createClientIdAndSecret,
-  createToken,
-  verifyClientId,
-  verifyClientSecret,
+	createClientIdAndSecret,
+	createToken,
+	verifyClientId,
+	verifyClientSecret,
 } from "./lib/auth";
 import { IClientIdAndSecret } from "./lib/types";
 
 class OpenAuth {
-  private static generateToken(secret: string): string {
-    return createToken(secret);
-  }
+	secret: string;
 
-  generateClientIdAndSecret(secret: string): IClientIdAndSecret {
-    return createClientIdAndSecret(secret);
-  }
+	constructor(secret: string) {
+		if (secret.length < 8 || secret.length > 30)
+			throw Error("Length of secret should be between 8 - 30");
+		this.secret = secret;
+	}
+	private static generateToken(secret: string): string {
+		return createToken(secret);
+	}
 
-  verifyClient(
-    clientId: string,
-    clientSecret: string,
-    secret: string
-  ): string | Error {
-    const timestamp = clientSecret.slice(
-      clientSecret.length - 44,
-      clientSecret.length
-    );
-    const token = verifyClientId(timestamp, secret);
-    const ClientSecretData = verifyClientSecret(clientId, clientSecret);
-    if (token === ClientSecretData) {
-      return OpenAuth.generateToken(secret);
-    }
-    throw new Error("Authentication failed");
-  }
+	generateClientIdAndSecret(): IClientIdAndSecret {
+		return createClientIdAndSecret(this.secret);
+	}
 
-  verifyToken = (token: string, secret: string) => {
-    const generateToken = createToken(secret);
-    if (generateToken === token) return true;
-    return false;
-  };
+	verifyClient(clientId: string, clientSecret: string): string | Error {
+		if (typeof clientId !== "string") throw Error("clientId should be string");
+
+		const timestamp = clientSecret.slice(
+			clientSecret.length - 44,
+			clientSecret.length
+		);
+		const token = verifyClientId(timestamp, this.secret);
+		const ClientSecretData = verifyClientSecret(clientId, clientSecret);
+		if (token === ClientSecretData) {
+			return OpenAuth.generateToken(this.secret);
+		}
+		throw new Error("Authentication failed");
+	}
+
+	verifyToken(token: string): boolean | Error {
+		if (typeof token !== "string") throw Error("token should be string");
+
+		const generateToken = createToken(this.secret);
+		if (generateToken === token) return true;
+		return false;
+	}
 }
 
 export default OpenAuth;
